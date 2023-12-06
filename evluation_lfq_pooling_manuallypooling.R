c4b4ba24b05781447944908de465e9413692867d
david <lilek@tulln.fhwn.ac.at>
2023-12-06 13:14:26 +0100

    updates

---
+++ evluation_lfq_pooling_manuallypooling.R
@@ -0,0 +1,40 @@
+1471/1970
+#nur 75% proteine mit lfq bei qc when manuell gepooled und mbr
+1210/1970
+#nur 61% proteine mit lfq bei qc when manuell gepooled und kein mbr
+
+## extracts
+661/653
+529/535
+686/650
+538/544
+446/413
+331/322
+496/469
+354/358770/753
+609/595
+807/806
+647/659
+
+d<-readRDS(file = "results_v002/results_run1_mqpar_extracts_2gether_E4_D1_AF_TF_LFQ_combined_txt_proteinGroups.txt_LFQ.RDS")
+d <- d[[1]]
+
+#B
+723+79+416+74
+#AF
+74+416+54+28
+#TF
+416+54+79+14
+
+
+
+sum <- 501+344+20
+344/sum
+501/sum
+20/sum
+
+
+sum <- 768+524+65
+768/sum
+524/sum
+65/sum
